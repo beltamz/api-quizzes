872756af0a0748bdbe08029d6a0699b672d5a446
Belen Zelaya <belenzelaya21@gmail.com>
2025-09-29 22:28:15 -0300

    Updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from './config/database';
 import userRoutes from './routes/userRoutes';
 import quizRoutes from './routes/quizRoutes';
 import cors from 'cors';
+import path from "path";
 
 const app= express()
 
@@ -17,6 +18,14 @@ app.use(express.json())
 //Rutas
 app.use('/api/usuarios', userRoutes)
 app.use('/api/quizzes', quizRoutes)
+
+// Servir los archivos estáticos de React
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Todas las demás rutas que no sean API, servirán index.html
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 connectDB()
 
