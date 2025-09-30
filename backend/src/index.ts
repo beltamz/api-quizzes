9c85b126bd16f7aa668f2948f73b78feef33e6c4
Belen Zelaya <belenzelaya21@gmail.com>
2025-09-29 22:57:32 -0300

    Updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,9 @@
-import path from "path";
 import 'dotenv/config';
 import express, {Request, Response} from 'express';
 import connectDB from './config/database';
 import userRoutes from './routes/userRoutes';
 import quizRoutes from './routes/quizRoutes';
 import cors from 'cors';
-
 
 const app= express()
 
@@ -20,20 +18,12 @@ app.use(express.json())
 app.use('/api/usuarios', userRoutes)
 app.use('/api/quizzes', quizRoutes)
 
-// Servir los archivos estáticos de React
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-// Todas las demás rutas que no sean API, servirán index.html
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
-
 connectDB()
 
 //ruta que lee formato json
-/*app.get("/", (req: Request,res: Response)=>{
+app.get("/", (req: Request,res: Response)=>{
     res.send("API funcionando");
-})*/
+})
 
 //Inicio de servidor
 const PORT = process.env.PORT || 3000
